d41185ec1a63c5f4a33b946744db6252523895e9
Big-Soft-Doggo <thelifeofone09@gmail.com>
2024-10-16 12:09:03 -0500

    Additional Changes: -Added multiple new items such as: Sapphire,Raw Sapphire, Fey Dew, and dough! -Created a mod inventory tab. -Added the places for new items to be codded in! -Added EN localization files.

--- src/main/java/net/nyx_whitefox/canisalterna/datagen/ModBlockTagGenerator.java
+++ src/main/java/net/nyx_whitefox/canisalterna/datagen/ModBlockTagGenerator.java
@@ -45,6 +45,12 @@ public class ModBlockTagGenerator extends BlockTagsProvider {
         this.tag(Tags.Blocks.NEEDS_NETHERITE_TOOL)
         ;
 
+        this.tag(BlockTags.FENCES)
+                .add(ModBlocks.SAPPHIRE_FENCE.get());
+        this.tag(BlockTags.FENCE_GATES)
+                .add(ModBlocks.SAPPHIRE_FENCE_GATE.get());
+        this.tag(BlockTags.WALLS)
+                .add(ModBlocks.SAPPHIRE_WALL.get());
 
         this.tag(BlockTags.NEEDS_DIAMOND_TOOL)
                 .add(ModBlocks.END_STONE_SAPPHIRE_ORE.get());
--- src/main/java/net/nyx_whitefox/canisalterna/datagen/ModItemModelProvider.java
+++ src/main/java/net/nyx_whitefox/canisalterna/datagen/ModItemModelProvider.java
@@ -3,9 +3,11 @@ package net.nyx_whitefox.canisalterna.datagen;
 import net.minecraft.data.PackOutput;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
 import net.minecraftforge.client.model.generators.ItemModelBuilder;
 import net.minecraftforge.client.model.generators.ItemModelProvider;
 import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
 import net.nyx_whitefox.canisalterna.CanisAlterna;
 import net.nyx_whitefox.canisalterna.item.ModItems;
@@ -35,6 +37,21 @@ public class ModItemModelProvider extends ItemModelProvider {
         return withExistingParent(item.getId().getPath(),
                 new ResourceLocation("item/generated")).texture("layer0",
                 new ResourceLocation(CanisAlterna.MOD_ID,"item/" + item.getId().getPath()));
+    }
+
+    public void fenceItem(RegistryObject<Block> block, RegistryObject<Block> baseBlock) {
+        this.withExistingParent(ForgeRegistries.BLOCKS.getKey(block.get()).getPath(), mcLoc("block/fence_inventory"))
+                .texture("texture",  new ResourceLocation(CanisAlterna.MOD_ID, "block/" + ForgeRegistries.BLOCKS.getKey(baseBlock.get()).getPath()));
+    }
+
+    public void buttonItem(RegistryObject<Block> block, RegistryObject<Block> baseBlock) {
+        this.withExistingParent(ForgeRegistries.BLOCKS.getKey(block.get()).getPath(), mcLoc("block/button_inventory"))
+                .texture("texture",  new ResourceLocation(CanisAlterna.MOD_ID, "block/" + ForgeRegistries.BLOCKS.getKey(baseBlock.get()).getPath()));
+    }
+
+    public void wallItem(RegistryObject<Block> block, RegistryObject<Block> baseBlock) {
+        this.withExistingParent(ForgeRegistries.BLOCKS.getKey(block.get()).getPath(), mcLoc("block/wall_inventory"))
+                .texture("wall",  new ResourceLocation(CanisAlterna.MOD_ID, "block/" + ForgeRegistries.BLOCKS.getKey(baseBlock.get()).getPath()));
     }
 }
 
